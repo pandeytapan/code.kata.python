81f6e7523d1f0dea37184a29309a10a601edaeae
Tapan Pandey <tapan@programmingdays.com>
2020-03-22 19:16:10 +0530

    p1.1

---
+++ chapter.01/p1.1.py
@@ -0,0 +1,9 @@
+# program that prints a greeting in a language other than English.
+# The program below prints नमस्ते.
+# This works in Python 3.x.x
+
+# We have written each letter individually
+print("\u0928\u092E\u0938\u094D\u0924\u0947")
+
+# we can write the whole sentance also
+print("नमस्ते")
