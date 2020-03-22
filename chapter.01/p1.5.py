326acf47b52e2f0f378ad99877ba8858d3309830
Tapan Pandey <tapan@programmingdays.com>
2020-03-22 22:17:10 +0530

    p1.5

---
+++ chapter.01/p1.5.py
@@ -0,0 +1,6 @@
+# program that displays your name inside a box on the screen
+# First way. Using string interpolation
+print ("+" + "-" * 7 + "+")
+print ("| Tapan |")
+print ("+" + "-" * 7 + "+")
+
