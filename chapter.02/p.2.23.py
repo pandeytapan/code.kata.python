1194f0f486b7131e1cb0de297615824a27788155
Tapan Pandey <tapan@programmingdays.com>
2020-08-23 21:47:49 +0530

    p.2.23

--- chapter.02/p.2.23.py
+++ chapter.02/p.2.23.py
@@ -5,8 +5,12 @@ win = GraphicsWindow()
 canvas = win.canvas()
 
 # Draw in the canvas
-canvas.drawRect(5, 10 ,20, 30)
+canvas.setFill(255, 0 ,0)
+canvas.drawRect(0, canvas.height() // 4 , canvas.width(), canvas.height() // 2 )
 
+canvas.setOutline(255 ,255 ,255)
+
+canvas.drawText(10, canvas.height() // 4 + 10, "Hello World")
 win.wait()
 
 
