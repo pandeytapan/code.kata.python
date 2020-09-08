960b7bde67f2869e4ee300857fba119d6c3c65d1
Tapan Pandey <tapan@programmingdays.com>
2020-09-08 21:41:45 +0530

    p.2.26.py

--- chapter.02/p.2.26.py
+++ chapter.02/p.2.26.py
@@ -1,13 +1,15 @@
 from ezgraphics import GraphicsWindow
 
+fillname = ('black', 'white')
 window = GraphicsWindow()
 canvas = window.canvas()
 
 width= canvas.width()
 height = canvas.height()
-colorWhite = True
+colorWhite = 0
 for delta in range (0, width // 2, 40):
-    canvas.setFill("white" if colorWhite else "black")
+    canvas.setFill(fillname[colorWhite])
     canvas.drawOval(delta , delta, width - 2 * delta, height - 2 * delta)
+    colorWhite = not colorWhite
 
 window.wait()
