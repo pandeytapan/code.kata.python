13c436dea021c5905d157cabb8f39cd15f713ca2
Tapan Pandey <tapan@programmingdays.com>
2020-09-09 21:50:16 +0530

    p.2.27.py

--- chapter.02/p.2.27.py
+++ chapter.02/p.2.27.py
@@ -3,8 +3,12 @@ from ezgraphics import GraphicsWindow
 window = GraphicsWindow()
 
 canvas = window.canvas()
+height = canvas.height()
+width = canvas.width()
 
 canvas.setBackground(0, 0, 0)
 canvas.setOutline(0xFF, 0x00, 0xFF)
-canvas.drawRectangle(10, 10, 50, 50)
+canvas.drawRectangle(width / 4, height / 4, width / 2, height / 2)
+canvas.drawLine(width / 4, height / 4, width / 2, 0)
+canvas.drawLine(width / 2, 0, 3 * width / 4, height / 4)
 window.wait()
