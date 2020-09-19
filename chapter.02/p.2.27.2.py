a11491a3ccc38300a8bb32fe666dd5e17427508d
Tapan Pandey <tapan@programmingdays.com>
2020-09-19 22:07:03 +0530

    p.2.27.2.py

--- chapter.02/p.2.27.2.py
+++ chapter.02/p.2.27.2.py
@@ -7,21 +7,43 @@ height = canvas.height()
 width = canvas.width()
 
 canvas.setBackground(0, 0, 0)
-canvas.setOutline(0xFF, 0x00, 0xFF)
+canvas.setOutline(189,183,107)
 
-canvas.setFill("Yellow")
+# canvas.setFill("Yellow")
+# canvas.drawPoly(
+#     (width / 2, height / 4), 
+#     (width, height / 4), 
+#     (width, height / 2),
+#     (7 * width / 8, height / 2),
+#     (7 * width / 8, 3 * height / 4),
+#     (5 * width / 8, 3 * height / 4),
+#     (5 * width / 8, height / 2),
+#     (width / 2, height / 2),
+# )
+
+
+canvas.setFill(238,232,170)
 canvas.drawPoly(
-    (width / 2, height / 4), 
-    (width, height / 4), 
-    (width, height / 2),
+    (width / 4, 3 * height / 8),
+    (width / 2, height / 4),
+    (width, height / 4),
+    (3 * width / 4, 3 * height / 8),
+)
+canvas.drawPoly(
+    (5 * width / 8, 5 * height / 8),
     (7 * width / 8, height / 2),
     (7 * width / 8, 3 * height / 4),
-    (5 * width / 8, 3 * height / 4),
-    (5 * width / 8, height / 2),
-    (width / 2, height / 2),
+    (5 * width / 8, 7 * height / 8),
 )
 
-canvas.setFill("Green")
+canvas.drawPoly(
+    (3 * width / 4, 3 * height / 8),
+    (width , height / 4),
+    (width, height / 2),
+    (3 * width / 4, 5 * height / 8),
+)
+
+canvas.setFill(240,230,140)
 canvas.drawPoly(
     (width / 4, 3 * height / 8), 
     (3 * width / 4, 3 * height / 8), 
@@ -31,13 +53,5 @@ canvas.drawPoly(
     (3 * width / 8, 7 * height / 8),
     (3 * width / 8, 5 * height / 8),
     (width / 4,  5 * height / 8),)
-
-canvas.setFill("Red")
-canvas.drawPoly(
-    (width / 4, 3 * height / 8),
-    (width / 2, height / 4),
-    (width, height / 4),
-    (3 * width / 4, 3 * height / 8),
-)
 
 window.wait()
