647bd252dd3ceadf6cb30be65d206110fb9c9786
Tapan Pandey <pandey.tapan@hotmail.com>
2022-02-05 21:52:30 +0530

    Added python.testing

--- tee_tester/tee/020.py
+++
@@ -1,8 +0,0 @@
-from contextlib import nullcontext
-import sys
-
-with open(sys.argv[1], mode='wt') if len(sys.argv) > 1 else nullcontext() as f:
-    for line in sys.stdin:
-        sys.stdout.write(line)
-        if f:
-            f.write(line)
--- tee_tester/tee/030.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-f = open(sys.argv[1], mode='wt') if len(sys.argv) > 1 else None
-
-for line in sys.stdin:
-    print(line, end='')
-    if f:
-        print(line, end='', file=f)
