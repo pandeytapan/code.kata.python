90563047792f598abf352f591367678f5a75c947
Tapan Pandey <pandey.tapan@hotmail.com>
2023-05-21 22:37:47 +0530

    Completed script to create many files in many directories

--- misc.code/filemonk.py
+++ misc.code/filemonk.py
@@ -2,6 +2,7 @@
 
 import os
 from faker import Faker
+import random
 
 TWO_KIB_IN_BYTES = 2048
 # Ask for the number of folders to create
@@ -10,7 +11,18 @@ num_folders = int(input("How many folders you want to create: "))
 # Ask for the disk size to fill in bytes
 disk_size = int(input("Enter the size of the disk to fill (in bytes): "))
 
+if disk_size < TWO_KIB_IN_BYTES:
+    print("Disk size should be atleast 2KiB")
+    exit(1)
+
+# Calculate the number of files to create in each folder
 main_folder = "root"
+total_files = disk_size // TWO_KIB_IN_BYTES
+files_per_folder, extra_files = divmod(total_files, num_folders)
+
+if not files_per_folder:
+    print("Disk size is too small to create even one file in each folder")
+    exit(1)
 
 os.makedirs(main_folder, exist_ok=True)
 
@@ -20,33 +32,30 @@ for i in range(num_folders):
     # Generate a random folder name
     folder_name = fake.company().replace(" ", "_")
 
-    # Make the complete path from root till the folder name and create the
-    # folder
     folder_path = os.path.join(main_folder, folder_name)
     os.makedirs(folder_path, exist_ok=True)
     print(f"Created folder: {folder_path}")
 
     print(f"Creating files inside folder {folder_path}")
 
-    total_folder_size_in_bytes = 0
-    # Keep track of number of files inside the folder
-    max_files_in_a_folder = disk_size // (num_folders * TWO_KIB_IN_BYTES)
-
-    # Create files till the disk dize is remaining and the number of files
-    # remaining
-    while total_folder_size_in_bytes < disk_size and max_files_in_a_folder:
+    for j in range(files_per_folder):
         file_name = fake.file_name()
         file_path = os.path.join(folder_path, file_name)
 
-        # limit the file size 
-        file_size = min((disk_size - total_folder_size_in_bytes) // max_files_in_a_folder, 2048)
-        # Now create the file in the given path and fill atleast 2KiB of
-        # random data
+        # limit the file size
         with open(file_path, mode="w") as f:
-            paragraphs = [fake.paragraph() for _ in range(file_size // len(fake.paragraph()))]
-            f.write("\n".join(paragraphs))
+            textdata = fake.text(max_nb_chars=random.randint(100, TWO_KIB_IN_BYTES))
+            f.write(textdata)
 
         print(f"Created file: {file_path}")
 
-        total_folder_size_in_bytes += os.path.getsize(file_path)
-        max_files_in_a_folder -= 1
+for j in range(extra_files):
+    file_name = fake.file_name()
+    file_path = os.path.join(folder_path, file_name)
+
+    # limit the file size
+    with open(file_path, mode="w") as f:
+        textdata = fake.text(max_nb_chars=TWO_KIB_IN_BYTES)
+        f.write(textdata)
+
+    print(f"Created file: {file_path}")
