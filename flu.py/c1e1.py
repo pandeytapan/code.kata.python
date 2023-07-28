d6628e350be789f7f888ae8a621325116261b67b
Tapan Pandey <pandey.tapan@hotmail.com>
2023-07-28 22:22:27 +0530

    Added the french deck

--- flu.py/c1e1.py
+++ flu.py/c1e1.py
@@ -4,3 +4,4 @@ from collections import namedtuple
 # A named tuple can be used to create a tuple like class
 Card = namedtuple('Card', ['rank', 'suit'])
 
+# let us now create a french deck based on the deck
