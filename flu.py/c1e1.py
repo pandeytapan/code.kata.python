b236dedd49031a4f9b514523000bab24e3e78798
Tapan Pandey <pandey.tapan@hotmail.com>
2023-07-29 22:01:45 +0530

    Completed the french_deck

--- flu.py/c1e1.py
+++ flu.py/c1e1.py
@@ -5,3 +5,23 @@ from collections import namedtuple
 Card = namedtuple('Card', ['rank', 'suit'])
 
 # let us now create a french deck based on the deck
+
+class french_deck:
+    ranks = [position for position in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    # initialize the french deck
+    # _cards is basically a list of Card type
+    def __init__(self):
+        self._cards = [Card(rank,suit) 
+                       for suit in self.suits 
+                       for rank in self.ranks]
+    
+    # Get the total number of cards in the deck
+    def __len__(self):
+        return len(self._cards)
+
+    # Get the card at a particular position
+    # We basically are returning particular item from the list
+    def __getitem__(self, pos):
+        return self._cards[pos]
