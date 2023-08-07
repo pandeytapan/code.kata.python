ffe7eb8c798798918d0918beea86c133e88073d9
Tapan Pandey <pandey.tapan@hotmail.com>
2023-08-07 22:00:37 +0530

    Vector example added

--- flu.py/c1e1.py
+++ flu.py/c1e1.py
@@ -4,6 +4,9 @@ from collections import namedtuple
 # A named tuple can be used to create a tuple like class
 Card = namedtuple('Card', ['rank', 'suit'])
 
+# Value of the suit 
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
 # let us now create a french deck based on the deck
 
 class french_deck:
@@ -23,5 +26,14 @@ class french_deck:
 
     # Get the card at a particular position
     # We basically are returning particular item from the list
+    # with __getitem__ implemented we can now support the slicing also
+    # The `in` operator requires __contains__, but in case it is not there
+    # it uses __getitem__ and traverses sequentially.
     def __getitem__(self, pos):
         return self._cards[pos]
+
+# Function to rank each of the individual card in the suit
+def card_rank(some_card):
+    rank_of_card = french_deck.ranks.index(some_card.rank)
+    rank_of_card = rank_of_card * len(suit_values) + suit_values[some_card.suit] 
+    return rank_of_card
---
+++ flu.py/c1e2.py
@@ -0,0 +1,26 @@
+from math import hypot
+
+class vector:
+    '''The Vector class'''
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Vector({self.x}, {self.y})'
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, o):
+        x = self.x + other.x
+        y = self.y + other.y
+        return vector(x, y)
+
+    def __mul__(self, s):
+        return vector(self.x * s, self.y *s)
+
