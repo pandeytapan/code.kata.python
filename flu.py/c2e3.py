72b0c3f2c05961b30d269b69bd16c085e7dd1cb9
Tapan Pandey <pandey.tapan@hotmail.com>
2023-08-25 20:58:33 +0530

    Started with the slicing operator

--- flu.py/c2e3.py
+++ flu.py/c2e3.py
@@ -30,3 +30,13 @@ print(tokyo[2])
 # 1. _make() creates a namedtuple
 # 2. _asdict() returns a Ordereddictionary
 # 3. _fields that returns the field in the tuple
+LatLong = namedtuple('LatLong', 'lat long')
+# Now let us create a tuple using the LatLong named tuple
+delhi_ncr = ('Delhi NCR', 'IN', 21.472, LatLong(28.613889,77.208889))
+
+# Next we create a City type namedtuple using the delhi_ncr
+delhi = City._make(delhi_ncr)
+print(delhi)
+
+# Let's get a OrderedDict out of the namedtuple
+print(delhi._asdict())
---
+++ flu.py/c2e4.py
@@ -0,0 +1,7 @@
+# In this section we are going to see the slicing feature of the Python
+# Slice in itself is basically a object type in the Python
+
+s = 'Bicycle'
+
+print(s[::1])
+print(s[::-1])
