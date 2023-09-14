99443c838a7bf305fac11d9bc1efa35cdd148825
Tapan Pandey <pandey.tapan@hotmail.com>
2023-09-14 20:30:31 +0530

    Added new solution c2e8.py

--- flu.py/c2e7.py
+++ flu.py/c2e7.py
@@ -14,4 +14,14 @@ with open('floats.bin', 'wb') as f:
     floats.tofile(f)
 
 
+# A memoryview is a nice way to share the same set of memory without copying the bytes
 
+numbers = array('h', [-2, -1, 0, 1, 2])
+memv = memoryview(numbers) # This creates a memoryview of the numbers type
+
+print(memv[0])
+
+memv_oct = memv.cast('B')
+print(memv_oct.tolist())
+numbers[0] = -3
+print(memv[0])
