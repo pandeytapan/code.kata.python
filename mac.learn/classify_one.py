5b5e7af293de795d0694a4f5d0060ad25463656d
Tapan Pandey <pandey.tapan@hotmail.com>
2023-10-05 14:08:23 +0530

    DecisionTreeClassifier

---
+++ mac.learn/classify_one.py
@@ -0,0 +1,7 @@
+from sklearn.tree import DecisionTreeClassifier
+
+# Example data : [size, weight] and type of fruit
+data = [[120, 80], [100, 60], [130, 90], [150, 100]]
+
+# Example label : Type oof fruit. Apple or Orange
+labels = ['Apple', 'Apple', 'Orange', 'Orange']
