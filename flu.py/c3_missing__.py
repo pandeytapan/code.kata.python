941c421911f7377f48680092ac625c490114ded0
Tapan Pandey <pandey.tapan@hotmail.com>
2023-11-13 22:42:31 +0530

    Added a new file that talks of a dictionary that implements the missing element

---
+++ flu.py/c3_missing__.py
@@ -0,0 +1,5 @@
+#Implementing a dictionary that handles the missing key thing
+
+class StrKeyDict(dict):
+    '''A dictionary that converts the integer key to string one or else handles the missing key'''
+    
