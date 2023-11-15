0fd02194ace42f08c65afbe9d1bfda8d15010e12
Tapan Pandey <pandey.tapan@hotmail.com>
2023-11-15 22:33:40 +0530

    get added to the StrKeyDict

--- flu.py/c3_missing__.py
+++ flu.py/c3_missing__.py
@@ -2,4 +2,11 @@
 
 class StrKeyDict(dict):
     '''A dictionary that converts the integer key to string one or else handles the missing key'''
-    
+    def get(self, key, default=None):
+        print("StrKeyDict: __get__")
+        try:
+            return self[key]
+        except KeyError:
+            print("StrKeyDict: __caught__KeyError_")
+            return default
+
