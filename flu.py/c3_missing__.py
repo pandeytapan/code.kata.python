17802a623baf8a3ccac6929b67b245295a17d983
Tapan Pandey <pandey.tapan@hotmail.com>
2023-11-22 22:45:14 +0530

    Finalized the StrKeyMissing thing

--- flu.py/c3_missing__.py
+++ flu.py/c3_missing__.py
@@ -2,6 +2,14 @@
 
 class StrKeyDict(dict):
     '''A dictionary that converts the integer key to string one or else handles the missing key'''
+    def __missing__(self, key):
+        print("StrKeyDict:__missing__")
+        if isinstance(key, str):
+            # If the missing is is already an string we cannot handle it since str keys "need to be present if mentioned else KeyError
+            raise KeyError(key)
+        return self[str(key)]
+
+
     def get(self, key, default=None):
         print("StrKeyDict: __get__")
         try:
@@ -9,6 +17,7 @@ class StrKeyDict(dict):
         except KeyError:
             print("StrKeyDict: __caught__KeyError_")
             return default
+
 
     def __contains__(self, key):
         print("StrKeyDict: callback invoked __contains__")
