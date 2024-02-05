bc23596940728c14efd72e213034e1a98d51aefb
Tapan Pandey <pandey.tapan@hotmail.com>
2024-02-05 11:28:34 +0530

    Flake8 warning removal

--- machine.learn/microsoft.ai/lab/01-analyze-text/text-analysis.py
+++ machine.learn/microsoft.ai/lab/01-analyze-text/text-analysis.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 import os
-
-# Import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 
 def main():
@@ -12,31 +12,53 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
 
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(ai_key)
+        client = TextAnalyticsClient(
+            endpoint=ai_endpoint, credential=credential)
 
         # Analyze each text file in the reviews folder
-        reviews_folder = 'reviews'
+        reviews_folder = 'data'
         for file_name in os.listdir(reviews_folder):
             # Read the file contents
             print('\n-------------\n' + file_name)
-            text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
+            text = open(os.path.join(reviews_folder, file_name),
+                        encoding='utf8').read()
             print('\n' + text)
 
             # Get language
-
+            detected_language = client.detect_language(documents=[text])[0]
+            print('\nLanguage: ' + detected_language.primary_language.name)
 
             # Get sentiment
-
+            sentiment = client.analyze_sentiment(documents=[text])[0]
+            print('\nSentiment: ' + sentiment.sentiment)
 
             # Get key phrases
-
+            phrases = client.extract_key_phrases(
+                documents=[text])[0].key_phrases
+            if len(phrases) > 0:
+                print('\nKey Phrases:')
+                for phrase in phrases:
+                    print('\t{}'.format(phrase))
 
             # Get entities
-
+            entities = client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print('\nEntities:')
+                for entity in entities:
+                    print('\t{} ({})'.format(entity.text, entity.category))
 
             # Get linked entities
-
-
+            linked_entities = client.recognize_linked_entities(documents=[text])[
+                0].entities
+            if len(linked_entities) > 0:
+                print('\nLinked Entities:')
+                for linked_entity in linked_entities:
+                    print('\t{} ({})'.format(linked_entity.name,
+                          linked_entity.data_source_entity_id))
+                    print('\t\tURL: {}'.format(linked_entity.url))
+                    print('\t\tData Source: {}'.format(
+                        linked_entity.data_source))
 
     except Exception as ex:
         print(ex)
