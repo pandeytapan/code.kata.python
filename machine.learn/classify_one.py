2ebe6268cca25bb819a139ca4676b0fd66236122
Tapan Pandey <pandey.tapan@hotmail.com>
2024-02-23 22:18:32 +0530

    Removed non required files

--- machine.learn/classify_one.py
+++
@@ -1,5 +0,0 @@
-# Example data : [size, weight] and type of fruit
-data = [[120, 80], [100, 60], [130, 90], [150, 100]]
-
-# Example label : Type oof fruit. Apple or Orange
-labels = ['Apple', 'Apple', 'Orange', 'Orange']
