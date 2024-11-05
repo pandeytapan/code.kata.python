90f0a710801cbe7c3c6619b86bd3faf956b08fbc
Tapan Pandey <pandey.tapan@hotmail.com>
2024-11-05 21:54:07 +0530

    Decorators done

--- fluent/chap.07/code/htmldispatch.py
+++ fluent/chap.07/code/htmldispatch.py
@@ -1,6 +1,7 @@
 from functools import singledispatch
 import html
 from typing import Any
+import numbers
 
 # singledispatch marks the function that will handle the base type
 
@@ -9,3 +10,14 @@ from typing import Any
 def htmlize(obj: Any):
     content = html.escape(repr(obj))
     return '<pre>{}</pre>'.format(content)
+
+
+@htmlize.register(str)
+def _(text):
+    content = html.escape(text).replace('\n', '<br>\n')
+    return '<pre>{0}</pre>'.format(content)
+
+
+@htmlize.register(numbers.Integral)
+def _(n):
+    return '<pre>{0} (0:{0:x})</pre>'.format(n)
